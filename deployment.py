7c1b818c00f0b38fbf951267b60dc89a33efe3a5
ShihabYasin <shihabyasin@gmail.com>
2022-05-07 05:41:10 +0600

    currentDate

---
+++ deployment.py
@@ -0,0 +1,17 @@
+import glob
+import os
+import shutil
+import time
+
+txtfiles = []
+for file in glob.glob("pending_deployments/*"):
+    txtfiles.append(file)
+
+txtfiles = sorted (txtfiles)
+for idx, file in enumerate(txtfiles):
+    shutil.copy (file, '_posts/')
+    os.system ('./git_commit_auto_script.sh')
+    # time.sleep(45)
+    print (idx, file)
+    break
+
